68345497ce8672d704139d88c1b7f366c5bb4e00
Marcos Oliveira <ziguifrido@users.noreply.github.com>
2026-04-11 23:23:42 -0400

    Initializing projects

---
+++ src/test/java/com/algaworks/algasensors/temperature/monitoring/TemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.temperature.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
